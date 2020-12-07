c2d1e0f6b3cdae249b6544ce2b0cd168f222098e
Sydney Waters <saw465712@uga.edu>
2020-12-07 11:28:39 -0500

    Update ContentView.swift

--- Waters_InClass/ContentView.swift
+++ Waters_InClass/ContentView.swift
@@ -9,13 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var slide: Double = 0.0
+    @State private var counter = 0
+    var colors = [Color.green, Color.red, Color.blue, Color.yellow]
+    
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 40.0) {
+            Slider(value: $slide, in: 0.00001...360, step: 0.1)
+            Text("\(slide)")
+            ButtonsView(counter : $counter, colors : colors)
+            GraphView(slide : $slide, counter : $counter, color : colors[counter])
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GraphView: View {
+    @Binding var slide : Double
+    @Binding var counter : Int
+    var color : Color
+    
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 200))
+            path.addArc(center: .init(x :200, y:200), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: slide), clockwise: true)
+        }
+        .fill(color)
+    }
+}
+
+struct ButtonsView: View {
+    
+    @Binding var counter : Int
+    var colors : Array<Any>
+    
+    var body: some View {
+        HStack(spacing: 50.0) {
+            Button(action : {
+                self.counter-=1
+                if(self.counter == 0) {
+                    self.counter =  self.colors.count
+                }
+            }) {
+                Image(systemName: "arrow.left.circle")
+                    .font(.system(size: 35))
+            }
+            Button(action : {
+                self.counter+=1
+                if(self.counter == self.colors.count) {
+                    self.counter =  0
+                }
+            }) {
+                Image(systemName: "arrow.right.circle")
+                    .font(.system(size: 35))
+            }
+        }
     }
 }
